f1918a6a4ef85996919caaacad7a294d00568305
Syed Hamza Ali <syedhamzaale@gmail.com>
2024-08-21 01:59:39 +0500

    Proper Nuke

--- backend/models/charactermotives.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type CharacterMotives struct {
-	PersonalityTraits string `bson:"personalitytraits"`
-	Ideals            string `bson:"ideals"`
-	Bonds             string `bson:"bonds"`
-	Flaws             string `bson:"flaws"`
-}
