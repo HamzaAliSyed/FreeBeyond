d068989e04cb88b8e9dcd41eabceefc0b156cee5
Syed Hamza Ali <syedhamzaale@gmail.com>
2024-09-09 11:14:56 +0500

    This is final nuke, I promise

--- backend/database/databasemanagement.go
+++ backend/database/databasemanagement.go
@@ -12,14 +12,6 @@ import (
 
 var MongoClient *mongo.Client
 var Characters *mongo.Collection
-var Sources *mongo.Collection
-var Spells *mongo.Collection
-var Classes *mongo.Collection
-var Items *mongo.Collection
-var ArtisianTools *mongo.Collection
-var SubClasses *mongo.Collection
-var Races *mongo.Collection
-var ClassFeatures *mongo.Collection
 
 func ConnectToMongo() {
 	MongoClientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -38,13 +30,5 @@ func ConnectToMongo() {
 
 	fmt.Println("Connected to MongoDB!")
 
-	Sources = MongoClient.Database("DND").Collection("Sources")
-	Spells = MongoClient.Database("DND").Collection("Spells")
-	Classes = MongoClient.Database("DND").Collection("Classes")
-	Items = MongoClient.Database("DND").Collection("Items")
-	ArtisianTools = MongoClient.Database("DND").Collection("ArtisianTools")
-	SubClasses = MongoClient.Database("DND").Collection("SubClasses")
 	Characters = MongoClient.Database("DND").Collection("Characters")
-	Races = MongoClient.Database("DND").Collection("Races")
-	ClassFeatures = MongoClient.Database("DND").Collection("Class Features")
 }
