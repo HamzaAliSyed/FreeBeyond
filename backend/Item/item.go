b862e6cbdd104c30c01269f928d47684f3d0a36f
Syed Hamza Ali <syedhamzaale@gmail.com>
2024-09-20 06:37:46 +0500

    Created a Weapon Item type and a weapon factory

---
+++ backend/Item/item.go
@@ -0,0 +1,53 @@
+package item
+
+import "errors"
+
+type CreateParams interface{}
+
+type Item interface {
+	Create(params CreateParams) error
+	Print()
+}
+
+type Rarity string
+
+const (
+	Mundane   Rarity = "Mundane"
+	Common    Rarity = "Common"
+	Uncommon  Rarity = "Uncommon"
+	Rare      Rarity = "Rare"
+	VeryRare  Rarity = "Very Rare"
+	Legendary Rarity = "Legendary"
+	Artifact  Rarity = "Artifact"
+)
+
+func IsValidRarity(r Rarity) bool {
+	switch r {
+	case Mundane, Common, Uncommon, Rare, VeryRare, Legendary, Artifact:
+		return true
+	}
+	return false
+}
+
+type BasicParameter struct {
+	name        string   `bson:"name"`
+	typeTags    []string `bson:"typetags"`
+	description string   `bson:"description"`
+	rarity      Rarity   `bson:"rarity"`
+	cost        string   `bson:"cost"`
+	weight      string   `bson:"weight"`
+	source      string   `bson:"source"`
+}
+
+func CreateItemFactory(itemType string, params CreateParams) (Item, error) {
+	switch itemType {
+	case "weapon":
+		{
+			weapon := &Weapon{}
+			weaponCreateError := weapon.Create(params)
+			return weapon, weaponCreateError
+		}
+	}
+
+	return nil, errors.New("invalid Item Type")
+}
--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	character "backend/Character"
+	item "backend/Item"
+	"fmt"
 )
 
 func main() {
@@ -24,4 +26,29 @@ func main() {
 	}
 
 	HazaristSchaffenhifer.PrintCharacterSheet()
+
+	params := item.WeaponParams{
+		Name:           "Longsword",
+		Rarity:         item.Common,
+		Description:    "A longsword is a versatile, double-edged weapon characterized by its straight, extended blade, typically around 35 to 45 inches in length. \nDesigned for both cutting and thrusting, it is wielded with two hands, offering a balance between speed and power. \nIt is used by knights and adventures all over the Faerun and excels in close quarters combat.",
+		TypeTags:       []string{"Martial Weapon", "Melee Weapon"},
+		WeaponProperty: []string{"Versatile"},
+		DamageType:     []item.Damage{item.Slashing},
+		DamageAmount:   []string{"1d8"},
+		Cost:           "15 gp",
+		Weight:         "3 lbs",
+		Source:         "Player's Handbook",
+		RangeMin:       0,
+		RangeMax:       5,
+	}
+
+	Longsword, LongswordCreationError := item.CreateItemFactory("weapon", params)
+
+	if LongswordCreationError != nil {
+		fmt.Println(LongswordCreationError.Error())
+		return
+	}
+
+	Longsword.Print()
+
 }
