face9d2c8b6eff29a1327412967ea8bebfc60b7b
Hamza Ali Syed <syedhamzaale@gmail.com>
2025-03-05 00:52:01 +0500

    Restructureing the design to be more rigied

--- backend/internal/models/charactermodel.go
+++ backend/internal/models/charactermodel.go
@@ -2,41 +2,24 @@ package models
 
 import (
 	"backend/pkg/utils"
-	"fmt"
 )
 
 type Character struct {
-	Name          string
-	AbilityScores map[string]interface{}
-	Attacks       []Attack
+	Name string
 }
 
 type CharacterParameters struct {
-	Name                         string
-	AbilityScoresParametersArray []AbilityScoreParameters
+	Name string
 }
 
 func NewCharacter(parameters CharacterParameters) (Character, error) {
 	validName, validNameError := utils.ValidateName(parameters.Name)
 	var character Character
-	character.AbilityScores = make(map[string]interface{})
 	if validNameError != nil {
 		return character, validNameError
 	}
 
-	for _, abilityScore := range parameters.AbilityScoresParametersArray {
-		var characterAbility AbilityScore
-		asi, asm, abilityScoreUpdateError := utils.NewCharacterValidateScoreAndGenerateModifier(abilityScore.Score)
-		if abilityScoreUpdateError != nil {
-			fmt.Printf("cannot create attribute score %v", abilityScoreUpdateError)
-			return character, abilityScoreUpdateError
-		}
-
-		characterAbility.Name = abilityScore.Name
-		characterAbility.Score = asi
-		characterAbility.Modifier = asm
-		character.AbilityScores[abilityScore.Name] = characterAbility
-	}
 	character.Name = validName
+
 	return character, nil
 }
